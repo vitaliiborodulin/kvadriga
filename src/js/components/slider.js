a72527ce5c6a9472d1b78ab03ff48f1dd6b51efb
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2024-06-06 17:57:07 +0300

    update 06.06.2024

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,28 +1,52 @@
 // withus
-function navigationFill() {  
+// function navigationFill() {  
 
-    var pr = $('.f-carousel__dots li .f-carousel__dot');
-    $(pr).css({ width: "0%" });
+//     var pr = $('.f-carousel__dots li .f-carousel__dot');
+//     $(pr).css({ width: "0%" });
 
-    var progressbar = $('.f-carousel__dots li.is-current .f-carousel__dot');
-    $(progressbar).animate({ width: "100%" }, 'slow');
-}
+//     var progressbar = $('.f-carousel__dots li.is-current');
+//     $(progressbar).animate({ width: "100%" }, 'slow');
+// }
 
 const withus = document.querySelector('.withus__slider');
 
 if(withus) {
-    const withusSlider = new Carousel(withus, { 
+    const withusSlider = new Carousel(withus, {
+        Panzoom: {
+            decelFriction: 0.3,
+            // touch: false
+          },
+        // initialSlide: 0,
         // infinite: false,
         Navigation: false,
         // Dots: false,
+        slidesPerPage: 1,
         Autoplay : {
             timeout : 2000
         },
+        // transition: false,
         on: {
-            change: navigationFill
+            // change: navigationFill,
+            // change: (instance) => {
+            //     // Current page
+            //     const page = instance.page;
+            //     // Page count
+            //     const pages = instance.pages.length;
+            //     // Current page slides
+            //     const slides = instance.pages[page].slides;
+            //     console.log(page)
+            //   },
         }
     }, {Autoplay});
     // });
+
+    $('.withus__next').on('click', function(){
+        withusSlider.slideNext();
+    });
+
+    $('.withus__prev').on('click', function(){
+        withusSlider.slidePrev();
+    });
 }
 
 // service slider
@@ -53,5 +77,16 @@ if(ss) {
     
     $('.ss__filter-buttons button').on('click', function(){
         ssSlider.reInit();
+    });
+}
+
+// snews slider
+const ns = document.querySelector('.news__slider');
+
+if(ns) {
+    var newsSlider = new Carousel(ns, { 
+        // Navigation: false,
+        Dots: false,
+        infinite : false,
     });
 }
