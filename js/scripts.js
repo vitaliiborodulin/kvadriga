1b8bfb1d9fc8e86665149a39341d19c0c6f06486
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2024-06-06 13:15:26 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -169,30 +169,54 @@ Fancybox.bind('[data-fancybox="gallery"]', {
     },
 });
 // withus
-function navigationFill() {  
+// function navigationFill() {  
 
-    var pr = $('.f-carousel__dots li .f-carousel__dot');
-    $(pr).css({ width: "0%" });
+//     var pr = $('.f-carousel__dots li .f-carousel__dot');
+//     $(pr).css({ width: "0%" });
 
-    var progressbar = $('.f-carousel__dots li.is-current .f-carousel__dot');
-    $(progressbar).animate({ width: "100%" }, 'slow');
-}
+//     var progressbar = $('.f-carousel__dots li.is-current');
+//     $(progressbar).animate({ width: "100%" }, 'slow');
+// }
 
 const withus = document.querySelector('.withus__slider');
 
 if(withus) {
-    const withusSlider = new Carousel(withus, { 
+    const withusSlider = new Carousel(withus, {
+        Panzoom: {
+            decelFriction: 0.3,
+            // touch: false
+          },
+        // initialSlide: 0,
         // infinite: false,
         Navigation: false,
         // Dots: false,
+        slidesPerPage: 1,
         Autoplay : {
             timeout : 2000
         },
+        // transition: false,
         on: {
-            change: navigationFill
+            // change: navigationFill,
+            // change: (instance) => {
+            //     // Current page
+            //     const page = instance.page;
+            //     // Page count
+            //     const pages = instance.pages.length;
+            //     // Current page slides
+            //     const slides = instance.pages[page].slides;
+            //     console.log(page)
+            //   },
         }
     }, {Autoplay});
     // });
+
+    $('.withus__next').on('click', function(){
+        withusSlider.slideNext();
+    });
+
+    $('.withus__prev').on('click', function(){
+        withusSlider.slidePrev();
+    });
 }
 
 // service slider
