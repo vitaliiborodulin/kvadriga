c81eed21b9968b821d2440da95980847b2369577
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2024-06-11 15:12:05 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -162,114 +162,72 @@ Fancybox.bind('[data-fancybox="gallery"]', {
         type: "classic",
     },
 });
-// withus
-// function navigationFill() {  
-
-//     var pr = $('.f-carousel__dots li .f-carousel__dot');
-//     $(pr).css({ width: "0%" });
-
-//     var progressbar = $('.f-carousel__dots li.is-current');
-//     $(progressbar).animate({ width: "100%" }, 'slow');
-// }
-
-const withus = document.querySelector('.withus__slider');
-
-if(withus) {
-    const withusSlider = new Carousel(withus, {
-        Panzoom: {
-            decelFriction: 0.3,
-            // touch: false
-          },
-        // initialSlide: 0,
-        // infinite: false,
-        Navigation: false,
-        // Dots: false,
-        slidesPerPage: 1,
-        Autoplay : {
-            timeout : 2000
-        },
-        // transition: false,
-        on: {
-            // change: navigationFill,
-            // change: (instance) => {
-            //     // Current page
-            //     const page = instance.page;
-            //     // Page count
-            //     const pages = instance.pages.length;
-            //     // Current page slides
-            //     const slides = instance.pages[page].slides;
-            //     console.log(page)
-            //   },
-        }
-    }, {Autoplay});
-    // });
-
-    $('.withus__next').on('click', function(){
-        withusSlider.slideNext();
-    });
-
-    $('.withus__prev').on('click', function(){
-        withusSlider.slidePrev();
-    });
-}
+// withus slider
+const withusSlider = new Swiper('.withus__slider', {
+    loop: true,
+    autoplay: {
+        delay: 5000,
+    },
+    slidesPerView: 1,
+    spaceBetween: 0,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
 
 // service slider
-const ss = document.querySelector('.ss__slider');
-
-if(ss) {
-    var ssSlider = new Carousel(ss, { 
-        Navigation: false,
-        Dots: false,
-        infinite : false,
-        dragFree: false
-        // 'slidesPerPage' : 2,
-        // 'friction' : 0.8,
-        // 'center': true, 
-        // Autoplay : {
-        //     timeout : 2000
-        // },
-        // on: {
-        //     change: (instance) => {
-        //         console.log(1)
-        //     }
-        // }
-    });
-
-    $(window).resize(function(){
-        ssSlider.reInit();
-    });
+const ssSlider = new Swiper('.ss__slider', {
+    // loop: true,
     
-    $('.ss__filter-buttons button').on('click', function(){
-        ssSlider.reInit();
-    });
-}
+    slidesPerView: 1.2,
+    spaceBetween: 16,
+    breakpoints: {
+        1200: {
+            slidesPerView: 3.2,
+            spaceBetween: 24,
+        },
+        894: {
+            slidesPerView: 1.6,
+            spaceBetween: 20,
+        },
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
+
+$(window).resize(function(){
+    ssSlider.update();
+});
+
+$('.ss__filter-buttons button').on('click', function(){
+    ssSlider.update();
+});
 
 // news slider
-const ns = document.querySelector('.news__slider');
-
-if(ns) {
-    var newsSlider = new Carousel(ns, { 
-        // Navigation: false,
-        Dots: false,
-        infinite : false,
-    });
-}
+const newsSlider = new Swiper('.news__slider', {
+    loop: true,
+    slidesPerView: 1.2,
+    spaceBetween: 16,
+    breakpoints: {
+        768: {
+            slidesPerView: 2.5,
+            spaceBetween: 24,
+        },
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+});
 
 // case slider
-// const cs = document.querySelector('.case__slider');
-
-// if(cs) {
-//     var caseSlider = new Carousel(cs, { 
-//         // Navigation: false,
-//         dragFree: false,
-//         // Dots: false,
-//         infinite : false,
-//     });
-// }
-
 const caseSlider = new Swiper('.case__slider', {
-    // Optional parameters
-    // direction: 'vertical',
     // loop: true,
 
     slidesPerView: 1.1,
@@ -328,7 +286,7 @@ const caseSlider = new Swiper('.case__slider', {
 var licenseSlider = new Swiper('.license__slider', {
     loop: true,
     slidesPerView: 1.1,
-    spaceBetween: 0,
+    spaceBetween: 1,
     // breakpoints: {
     //     768: {
     //       slidesPerView: 1.2
