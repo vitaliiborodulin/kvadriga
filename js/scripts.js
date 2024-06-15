36c3363a8be964e3469cd00a2055e8d26d91b796
vitaliiborodulin <vitaliiborodulin@yandex.ru>
2024-06-15 12:15:39 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,28 @@
 $(function() {
 
+const burger = $('.mobile__burger');
+const closeMenu = $('.mobile__close');
+const menu = $('.mobile__menu');
 
+
+burger.on('click', function(e) {
+    e.preventDefault();
+    menu.addClass('open');
+    $('html, body').addClass('overflow');
+});
+
+closeMenu.on('click', function(e) {
+    e.preventDefault();
+    menu.removeClass('open');
+    $('html, body').removeClass('overflow');
+});
+
+$(document).keyup(function(e) {
+    if (e.key === "Escape" || e.keyCode === 27) {
+        menu.removeClass('open');
+        $('html, body').removeClass('overflow');
+    }
+});
 new ClipboardJS('.header__copy-btn');
 
 // var clipboardDemos=new ClipboardJS('.header__copy-btn');
